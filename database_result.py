b87ad54e89fec2b4c4ef5a8fe3f3f5b259071a1d
Howard-Pai <tmps9930715@gmail.com>
2025-04-14 01:53:13 -0700

    change db

--- database_result.py
+++ database_result.py
@@ -8,8 +8,7 @@ def mysql_runner(sql_query: str) -> str:
     # Connect to the database
     # Enter your database password here
     my_sql_password = input("Enter your MySQL password: ")
-    connection = f"mysql+pymysql://root:{my_sql_password}@localhost/banking"
+    connection = f"mysql+pymysql://root:{my_sql_password}@localhost"
     engine = create_engine(connection)
     output = pd.read_sql(sql_query, engine)
     print(output)
-
