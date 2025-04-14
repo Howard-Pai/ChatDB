ca65bb1618592c6842898bad42f3fc07439d4606
Howard-Pai <tmps9930715@gmail.com>
2025-04-14 01:49:02 -0700

    change output

--- database_result.py
+++ database_result.py
@@ -10,8 +10,6 @@ def mysql_runner(sql_query: str) -> str:
     my_sql_password = input("Enter your MySQL password: ")
     connection = f"mysql+pymysql://root:{my_sql_password}@localhost/banking"
     engine = create_engine(connection)
-    first_name = sys.argv[1]
-    last_name = sys.argv[2]
-    output = pd.read_sql(f"select AccountID, AccountType, Balance from account a left join customer c on a.CustomerID = c.CustomerID "
-                        f"where c.FirstName = '{first_name}' and c.LastName = '{last_name}'", engine)
+    output = pd.read_sql(sql_query, engine)
     print(output)
+
