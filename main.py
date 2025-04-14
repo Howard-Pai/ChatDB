3577510ea8c5075161573b41246a06374f79e002
Howard-Pai <tmps9930715@gmail.com>
2025-04-14 16:25:39 -0700

    update sample NL output

--- main.py
+++ main.py
@@ -12,17 +12,12 @@ if __name__ == "__main__":
     print(f"Command: {command}")
 
     # Call the function to convert NL command to SQL
-    # If the return command is supposed to be a SQL query, it should be in the format of a string
-    # If the return command is supposed to be a NoSQL query, it should be a JSON-formatted string with operation details
-    # The JSON string should be in the format:
-    # {
-    #     "collection": "collection_name",
-    #     "operation": "find",
-    #     "query": {"field": "value"},
-    #     "data": {"field": "value"},
-    #     "update": {"field": "new_value"}
-    # }
-    
+    # The return value is a tuple (db_type, command), where db_type is either "SQL" or "NoSQL" and command is a JSON-formatted string.
+    # If the command is a SQL command, it should specify the operation to be performed. operaitons include: show_tables, describe_table, sample_data, select, insert, update, delete
+    # Sample SQL command: {"operation": "list_tables"}, {"operation": "describe_table", "table": "table_name"}, {"operation": "sample_data", "table": "table_name", "limit": 5}, {"operation": "select", "query": "SELECT * FROM table_name"}
+    # If the command is a NoSQL command, it should specify the operation and the collection to be used. Operations include: list_collections, sample_data, find, aggregate, insert, update, delete
+    # Sample NoSQL command: {"operation": "list_collections"}, {"operation": "sample_data", "collection": "collection_name", "limit": 5}, {"operation": "find", "query": {"field": "value"}}, {"operation": "aggregate", "pipeline": [{"$match": {"field": "value"}}]}, {"operation": "insert", "data": {"field": "value"}}, {"operation": "update", "query": {"field": "value"}, "update": {"$set": {"field": "new_value"}}}, {"operation": "delete", "query": {"field": "value"}}
+
     db_type, command = convert_nl_to_sql(command)
 
     if command:
